1d021085132fbe0407799d35f7cd9bf5cf0d0192
SumonReign <sumonete152@gmail.com>
2024-08-01 12:54:49 +0600

    hi

---
+++ first_100_prime_numbers.js
@@ -0,0 +1,18 @@
+function isPrime(num) {
+    if (num <= 1) return false
+    for (let i = 2; i <= Math.sqrt(num); i++){
+         if (num % i == 0) return false
+
+      
+    }
+    return true
+}
+
+function printPrime(limit) {
+    for(let num = 0; num<= limit; num++){
+        if(isPrime(num)) console.log(num)
+    }
+}
+
+let limit = 100;
+printPrime(limit);
